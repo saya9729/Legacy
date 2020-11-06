f2607795a272759aeafc6934ebe0bd8cc5ea887f
nhvtdoox <nhutdoox@gmail.com>
2020-11-06 19:04:08 +0700

    animation for walk and stand is done

--- main.gd
+++ main.gd
@@ -11,6 +11,10 @@ const FRICTION = 500
 
 var velocity =  Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 # Called when the node enters the scene tree for the first time.func _process(delta):
 
 func _physics_process(delta):
@@ -20,24 +24,17 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 		
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Stand/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Stand")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity =  move_and_slide(velocity)
 	
-	
-	
-	if Input.is_action_just_pressed("ui_left"):
-		$AnimatedSprite.play("walk left")
-	if Input.is_action_just_released("ui_left"):
-		$AnimatedSprite.play("stand left")
-	
-	if Input.is_action_just_pressed("ui_right"):
-		$AnimatedSprite.play("walk right")
-	if Input.is_action_just_released("ui_right"):
-		$AnimatedSprite.play("stand right")
-	
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
