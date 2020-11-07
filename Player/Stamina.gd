99726c2d8f46f7dc34020a5d10d3a0ab96782d7c
longking0803 <50902413+longking0803@users.noreply.github.com>
2020-11-07 21:24:12 +0700

    55

--- Player/Stamina.gd
+++ Player/Stamina.gd
@@ -1,13 +1,11 @@
-extends CanvasLayer
+extends TextureProgress
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-func _input(event):
-	if event.is_action_pressed("GameMenu"):
-		get_node("GameMenu").show();
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,12 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Option_pressed():
-	pass # Replace with function body.
-
-
-func _on_Quit_pressed():
-	
-	get_node("GameMenu").hide();
