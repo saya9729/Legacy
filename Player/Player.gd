30bbd4dac621b49ea0e9afc91674eff756b163f1
nhvtdoox <nhutdoox@gmail.com>
2020-11-30 22:33:34 +0700

    refactor the state machine

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,15 +9,18 @@ var hp1
 var stamina1
 var hp_limit=100
 var stamina_limit=100
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 const ACCELERATION = 500
 const MAX_SPEED = 50
-const MAX_SPRINT_SPEED = 100
+const MAX_SPRINT_SPEED = 85
 const FRICTION = 500
 
+enum{
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
 var velocity =  Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -26,8 +29,14 @@ onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.func _process(delta):
 
-
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			pass
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = 2*(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
 	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
@@ -41,7 +50,7 @@ func _physics_process(delta):
 		animationState.travel("Walk")
 		
 		if Input.is_action_pressed("ui_sprint") && (Input.is_action_pressed("ui_up")||Input.is_action_pressed("ui_down")):
-			velocity = velocity.move_toward(input_vector * MAX_SPRINT_SPEED, ACCELERATION * delta)
+			velocity = velocity.move_toward(input_vector * MAX_SPRINT_SPEED * 0.75, ACCELERATION * delta)
 			stamina-=0.5
 		elif Input.is_action_pressed("ui_sprint") && (Input.is_action_pressed("ui_right")||Input.is_action_pressed("ui_left")):
 			velocity = velocity.move_toward(input_vector * MAX_SPRINT_SPEED, ACCELERATION * delta)
@@ -55,17 +64,17 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity =  move_and_slide(velocity)
-	
 
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-		
+
 func _ready():
 	timer.set_wait_time(1)
 	timer.start()
+	animationTree.active = true
 func _on_Timer_timeout():
 	timer2.set_wait_time(0.5)
 	timer2.start()
@@ -84,12 +93,12 @@ func save(save_game: Resource):
 		'health': hp,
 		'stamina': stamina,
 		'postion':  get_position(),
-	}
+}
+
 func _process(delta):
 	if hp==0:
 		var musicNode=$"Audio/Death"
 		musicNode.play()
-	
 
 func load(save_game: Resource):
 	var data: Dictionary = save_game.data[Save_key]
