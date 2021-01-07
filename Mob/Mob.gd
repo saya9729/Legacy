69f536d706e48dea627e513bc70317d6b6a2de7a
nhvtdoox <nhutdoox@gmail.com>
2021-01-08 00:13:12 +0700

    mob knockback

--- Mob/Mob.gd
+++ Mob/Mob.gd
@@ -3,8 +3,18 @@ extends KinematicBody2D
 
 onready var fly = $Fly
 
+const KNOCKBACK_SPEED = 170
+const KNOCKBACK_FRICTION = 350
+
+var knockback_direction = Vector2.ZERO
+var knockback_velocity = Vector2.ZERO
+
 func _physics_process(delta):
 	fly.play("Stand")
+	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, KNOCKBACK_FRICTION * delta)
+	knockback_velocity = move_and_slide(knockback_velocity)
 
 func _on_Hurtbox_area_entered(area):
-	queue_free()
+	knockback_direction = get_node("Hurtbox").global_position - area.global_position
+	knockback_direction = knockback_direction.normalized()
+	knockback_velocity = knockback_direction * KNOCKBACK_SPEED
