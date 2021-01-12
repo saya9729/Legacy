6304a1605f0bd9b4bc9016a80ebb6c11915bae4b
nhvtdoox <nhutdoox@gmail.com>
2021-01-12 23:07:13 +0700

    alo

--- Player/sTAMINABAR.gd
+++ Player/sTAMINABAR.gd
@@ -1,16 +1,4 @@
 extends TextureProgress
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-
-
 func _physics_process(delta):
 	value = get_tree().get_root().get_node("Node2D/YSort/Player").get("stamina")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
