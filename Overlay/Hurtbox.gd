9fef82fc80d239b5fec079a21fb1872c212e2b24
Lenomi <43492403+Lenomi@users.noreply.github.com>
2021-01-13 21:51:53 +0700

    Revert "Merge branch 'main' of https://github.com/saya9729/Legacy into main"
    This reverts commit e593e9ac191ceafc73a7c48b5b562298ecf98725, reversing
    changes made to 97a386d8785212c8ff9440dee80efd9105a27639.

--- Overlay/Hurtbox.gd
+++ Overlay/Hurtbox.gd
@@ -4,6 +4,5 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 
 func _on_Hurtbox_area_entered(area):
 	var hitEffect = HitEffect.instance()
-	var main = get_tree().current_scene
-	main.add_child(hitEffect)
+	get_parent().add_child(hitEffect)
 	hitEffect.global_position = global_position 
